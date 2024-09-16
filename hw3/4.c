084538797b616bb153809a0ee0b7b09d076e5ef5
themaestr00 <140151725+themaestr00@users.noreply.github.com>
2024-09-16 08:56:03 +0300

    Update 4.c

--- hw3/4.c
+++ hw3/4.c
@@ -9,7 +9,6 @@ satsum(int v1, int v2)
     {
         MY_INT_MIN = ~MY_INT_MAX
     };
-    printf("%d %d\n", MY_INT_MAX, MY_INT_MIN);
     if (v1 > 0 && v2 > 0 && MY_INT_MAX - v1 <= v2) {
         return MY_INT_MAX;
     } else if (v1 < 0 && v2 < 0 && v1 - MY_INT_MIN <= -v2) {
